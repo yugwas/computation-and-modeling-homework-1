f0e28690e3fe7e524430ce869e3794c78980b171
yugwas <76263462+yugwas@users.noreply.github.com>
2021-08-27 21:44:26 +0000

    get_intersection

--- 001/code.py
+++ 001/code.py
@@ -1,10 +1,26 @@
+# Monday and Tuesday code (8/23-8/24)
+
 txt = input("type here to see if your word is symmetrical: ")
 
 def check_for_symmetry(input_string):
     if input_string == (input_string [::-1]):
-       return "Word is symmetrical."
+       return True
     else:
-        return "Word is not symmetrical."
+        return False
 
 
 print(check_for_symmetry(txt))
+
+# Wednesday, Thursday, and Friday code (8/25-8/27)
+
+list1 = [100, 200, 500, 600, 900]
+list2 = [200, 400, 500, 800, 900]
+list3 = []
+
+def get_intersection(list1, list2):
+    for i in list1:
+        for y in list2:
+            if y == i:
+                list3.append(i)
+
+print(list3)
--- 001/tests.py
+++ 001/tests.py
@@ -1,9 +1,32 @@
+# Monday and Tuesday tests (8/23-8/24)
 
 def check_for_symmetry(input_string):
     if input_string == (input_string [::-1]):
-       return "Word is symmetrical."
+       return True
     else:
-        return "Word is not symmetrical."
+        return False
 
 print(check_for_symmetry('tacocat'))
 print(check_for_symmetry('leaf'))
+
+# Wednesday, Thursday, and Friday tests (8/25-8/27)
+
+list1 = [15, 2, 3, 4, 80, 69, 100, 20, 40]
+list2 = [30, 50, 60, 70, 100]
+list3 = []
+
+for i in list1:
+    for y in list2:
+        if y == i:
+            list3.append(i)
+
+print(list3)
+
+
+########
+#def get_intersection(list1, list2):
+#get_union
+#count_characters(input_string)
+
+#Notes from Justin: don't use any built in libraries, just do it from scratch. Also, don't use python sets.
+#
