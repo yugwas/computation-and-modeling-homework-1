f41285c0ed3072136e3f979e5f48e3602408c96e
Kristine M <76263462+yugwas@users.noreply.github.com>
2021-08-30 14:04:55 +0000

    i don't know how to do the last 4 functions

--- testing/base2.py
+++ testing/base2.py
@@ -1,12 +1,1 @@
 # convert to base 2 test code
-
-
-n = int(input("Enter decimal value: "))
-base_2 = ''
-
-while n != 0:
-    base_2 = base_2 + str(n%2)
-    n = n//2
-print ("Decimal value in Binary is: ")
-for i in range (len(base_2) -1, -1, -1):
-    print (base_2[i], end = '')
