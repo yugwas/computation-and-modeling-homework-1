6a15ceb817647ca6ba2c176860b05d3c64a40fc7
Kristine M <76263462+yugwas@users.noreply.github.com>
2021-08-30 22:36:01 +0000

    Removed some functions I didn't need.

--- 001/tests.py
+++ 001/tests.py
@@ -28,25 +28,6 @@ print(list3)
 #######################################################
 
 # get_union test code
-for i in list1:
-    for y in list2:
-        if y == i:
-            list1.remove(i)
-
-print(list1, list2)
-
-
-for i in list1:
-    for y in list2:
-        if y == i:
-            list2.remove(y)
-
-print(list1, list2)
-
-# both code above works without printing any repeats
-
-
-# now i need to make it into a function that prints out only 1 list
 
 list1 = [15, 20, 30, 45, 60, 70]
 list2 = [10, 15, 30, 60, 65, 80]
@@ -55,7 +36,6 @@ list3 = []
 getunion = list({i: i for i in list1 + list2}.values())
 
 print (getunion)
-# i rewrote the whole thing because I couldn't figure out how to combine the lists
 
 #######################################################
 
@@ -144,4 +124,3 @@ def convert_to_binary(decimal_num):
     return binary_num
 
 print(convert_to_binary(2))
-        
