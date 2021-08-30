a7212122c51fe0123f21c1fea8317a71a50783a2
Kristine M <76263462+yugwas@users.noreply.github.com>
2021-08-30 18:35:06 +0000

    Completed (almost) everything. Still missing converting to binary and formatting my code.

--- testing/base2.py
+++ testing/base2.py
@@ -1,1 +1,20 @@
 # convert to base 2 test code
+
+decimal_num = 19
+
+def convert_to_binary(decimal_num):
+    binary_num = ""
+    divided_num = decimal_num//2
+    remainder = divided_num % 2;
+    binary_num += str(remainder)
+    while(True):
+        if (divided_num == 0):
+            break;
+        divided_num = divided_num//2
+        remainder = divided_num % 2 
+        print(divided_num)
+        binary_num += str(remainder)
+    return binary_num
+
+print(convert_to_binary(19))
+        
