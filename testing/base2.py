d74ff5a081504af19a7413b8a0a143c0f695bc5b
Kristine M <76263462+yugwas@users.noreply.github.com>
2021-08-30 14:01:30 +0000

    trying base2

--- testing/base2.py
+++ testing/base2.py
@@ -1,4 +1,12 @@
 # convert to base 2 test code
 
 
-convert_to_base_2
+n = int(input("Enter decimal value: "))
+base_2 = ''
+
+while n != 0:
+    base_2 = base_2 + str(n%2)
+    n = n//2
+print ("Decimal value in Binary is: ")
+for i in range (len(base_2) -1, -1, -1):
+    print (base_2[i], end = '')
