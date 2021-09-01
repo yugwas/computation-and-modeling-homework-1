59febeaa94e477d7b529581b6f112f704124e9fe
Kristine M <76263462+yugwas@users.noreply.github.com>
2021-09-01 22:42:11 +0000

    Testing to se if my code even works anymore :/

--- 002/tests.py
+++ 002/tests.py
@@ -1,7 +1,1 @@
-
-message = ("This is my message!")
-
-def encoding_function(message):
-    if message != 0:
-        return 3*message - 4
-
+print ("test")
---
+++ 002/z.py
@@ -0,0 +1,8 @@
+#not my final code, i needed to put this somewhere in order to fix some files that weren't working
+
+#message = ("This is my message!")
+
+#def encoding_function(message):
+#    if message != 0:
+#        return 3*message - 4
+
