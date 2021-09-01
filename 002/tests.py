ae27a89cbe64034fd9b2302a37f8f1248a0882a1
Kristine M <76263462+yugwas@users.noreply.github.com>
2021-09-01 22:12:04 +0000

    Working on new code for this week.

--- 002/tests.py
+++ 002/tests.py
@@ -1,1 +1,11 @@
 
+message = ("This is my message!").dict
+
+def encoding_function(input):
+    input = []
+        if i != 0:
+            input.append(encoding_function(input))
+    return 3*input - 4
+
+print (encoding_function(message))
+
